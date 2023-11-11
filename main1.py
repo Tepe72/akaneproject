333238818127df65045f196a218e6bc1e24a1f28
Tepe72 <tepe.far.blue.72@gmail.com>
2023-11-11 18:46:14 +0900

    akaneプリント2nd

--- main1.py
+++ main1.py
@@ -1,19 +1,47 @@
-import os
-os.environ ['KIVY_GL_BACKEND'] = 'angle_sdl2'
-
 from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.label import Label
+from kivy.uix.button import Button
 
-from kivy.core.text import LabelBase, DEFAULT_FONT
-from kivy.resources import resource_add_path
 
-resource_add_path('C:\Windows\Fonts')
-LabelBase.register(DEFAULT_FONT, 'msmincho.ttc')
+class ScreenOne(Screen):
+    def __init__(self, **kwargs):
+        super(ScreenOne, self).__init__(**kwargs)
+        self.label = Label(text="画面1")
+        self.next_button = Button(text="次へ", on_press=self.switch_screen)
+
+        self.add_widget(self.label)
+        self.add_widget(self.next_button)
+
+    def switch_screen(self):
+        self.manager.current = 'screen_two'
+
+
+class ScreenTwo(Screen):
+    def __init__(self, **kwargs):
+        super(ScreenTwo, self).__init__(**kwargs)
+        self.label = Label(text="画面2")
+        self.next_button = Button(text="戻る", on_press=self.switch_screen)
+
+        self.add_widget(self.label)
+        self.add_widget(self.next_button)
+
+    def switch_screen(self):
+        self.manager.current = 'screen_one'
+
+
+class Main1App(App):
+    pass
+
+    def build(self):
+        sm = ScreenManager()
+        sm.add_widget(ScreenOne(name='screen_one'))
+        sm.add_widget(ScreenTwo(name='screen_two'))
+        return sm
+
 
 # App().run()
 
-class TextApp(App):
-    def build(self):
-        return Label(text='みなさんこんにちは')
 
-TextApp().run()
+if __name__ == '__main__':
+    Main1App().run()
